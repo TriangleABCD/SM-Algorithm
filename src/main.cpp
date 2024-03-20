ddb7ab934a532303a2e9362890f7cd7d16de758b
wangyaoyong <yaoyong.oerv@isrc.iscas.ac.cn>
2024-03-20 20:23:15 +0800

    nothing

--- src/main.cpp
+++ src/main.cpp
@@ -3,7 +3,7 @@
 #include "sm3.h"
 
 int main(int argc, char** argv) {
-    SM3 sm3("abc");
+    SM3 sm3("cd03e587d6ffb4a9e564693dd8eb83dd");
     auto hash = sm3.getHash();
     int k = 0;
     for(auto& c: hash) {
